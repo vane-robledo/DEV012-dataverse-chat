c99eb000a365f3c291e46cb046294cc75a7fc9df
Milton Di Simone <miltondisimone@gmail.com>
2023-11-06 13:40:02 -0300

    detalle

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,8 +4,9 @@ export const renderNav = () => {
   
   const homeIcon = document.createElement("button");
   homeIcon.id="button";
+  homeIcon.classList.add("homeButton")
   
-  homeIcon.innerHTML = `<img class= "home-button" src= "${"./img/home-button.png"}" alt="crew-img">`    
+  homeIcon.innerHTML = `<img class= "home-img" src= "${"./img/home-button.png"}" alt="crew-img">`    
   const nav = document.createElement("nav");
   nav.classList.add("navStyle");
   nav.appendChild(homeIcon);  
