771a4248462ea33dad8bbca63aefe8e4b36708ff
Yamileth Reveca <yamilethcaldera@gmail.com>
2023-11-21 18:33:23 -0300

    cambios chat grupal y chat individual y responsive

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { renderHome } from "./views/home.js";
 import { onURLChange, setRootElement, setRoutes } from "./router.js";
 import { error } from "./views/error.js";
 import { renderStats } from "./views/facts.js";
-import { renderH } from "./views/detalle.js";
+import { renderCharacters} from "./views/detalle.js";
 import { renderEveryone } from "./views/everyoneChat.js";
 import { renderApiKey } from "./apiKey.js";
 import dataset from "./data/dataset.js";
@@ -17,9 +17,9 @@ const routes = { "/": renderHome,
  "/api-key": renderApiKey,
 
 };
-// investigar como hacer la url dinamica 
+
 dataset.forEach((element) => {
-  routes[`/detalle-${element.id}`] = renderH;
+  routes[`/detalle-${element.id}`] = renderCharacters;
 });
 
 const rootRender = document.querySelector("#root");
